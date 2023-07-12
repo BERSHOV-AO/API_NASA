b03348348646a6808351cd0af4176da53c1b8aa8
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-07-12 14:31:00 +0300

    correction NASA.java

--- src/main/java/NASA.java
+++ src/main/java/NASA.java
@@ -2,19 +2,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class NASA {
 
-
-//    {
-//        "copyright": "Bill Peters",
-//            "date": "2020-07-17",
-//            "explanation": "After local midnight on July 14 comet NEOWISE was still above the horizon for Goldenrod, Alberta, Canada, just north of Calgary, planet Earth. In this snapshot it makes for an awesome night with dancing displays of the northern lights. The long-tailed comet and auroral displays are beautiful apparitions in the north these days. Both show the influence of spaceweather and the wind from the Sun. Skygazers have widely welcomed the visitor from the Oort cloud, though C/2020 F3 (NEOWISE) is in an orbit that is now taking it out of the inner Solar System.  Comet NEOWISE Images: July 16 | July 15 | July 14 | July 13 | July 12 | July 11 | July 10 & earlier",
-//            "hdurl": "https://apod.nasa.gov/apod/image/2007/DSC1028_PetersNEOWISEAuroralSpike.jpg",
-//            "media_type": "image",
-//            "service_version": "v1",
-//            "title": "NEOWISE of the North",
-//            "url": "https://apod.nasa.gov/apod/image/2007/DSC1028_PetersNEOWISEAuroralSpike_800.jpg"
-//    }
-
-
     private final String copyright;   // авторские права
     private final String date;
     private final String explanation; // объяснение
@@ -24,8 +11,6 @@ public class NASA {
     private final String title;
     private final String url;
 
-
-    //@JsonProperty("userId") int userId,
     public NASA(
             @JsonProperty("copyright") String copyright,
             @JsonProperty("date") String date,
