d00633589828ad2c0ceb542e197bee542d4c7a8c
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-07-19 09:08:16 +0300

    Modified Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,24 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
+/**
+ * Нужно воспользоваться публичным API NASA и скачать ежедневно выгружаемые им изображение или другой контент (например видео). Несмотря на то, что API публичный, доступ к нему предоставляется по ключу, который достаточно просто получить по адресу: https://api.nasa.gov/.
+ * <p>
+ * Перейдя по ссылке, заполняем личными данными поля: First Name, Last Name, Email и в ответ (а так же на почтовый адрес) будет выслан ключ. С этим ключом нужно делать запросы к API.
+ * <p>
+ * Итак, чтобы получить ссылку на картинку или другой контент, нужно:
+ * <p>
+ * Сделать запрос по адресу: https://api.nasa.gov/planetary/apod?api_key=ВАШ_КЛЮЧ
+ * Разобрать полученный ответ
+ * В ответе найти поле url - оно содержит адрес на изображение или другой контент (например видео), который нужно скачать и сохранить локально (на своем компьютере), имя сохраняемого файла нужно взять из части url (из примера ниже DSC1028_PetersNEOWISEAuroralSpike_800.jpg)
+ * Проверить что сохраненный файл открывается.
+ *
+ * key 1 : DO11mbGtFLwBtnbrzTcepxy9Z8b8bTGAxzRdkDXo
+ * key 2 : ma6vACpDJM8dk1jbY5VDjIb7y4zukGOudsMTWbej
+ */
+
+
 public class Main {
 
     public static final String REMOTE_SERVICE_URL = "https://api.nasa.gov/planetary/" +
